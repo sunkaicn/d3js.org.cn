2e23a9d0e685e853f3ba4557b66cad2ab69b2ed6
hueidou <hueidou163@gmail.com>
2015-11-29 01:53:09 +0800

    modify mongodb connection string

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,11 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+        uri: 'mongodb://' + process.env.D3_USERNAME 
+      + ':' + process.env.D3_PASSWORD 
+      + '@' + (process.env.D3_PORT_27017_TCP_ADDR || 'localhost') 
+      + ':' + process.env.D3_PORT_27017_TCP_PORT 
+      + '/mean-dev',
     options: {
       user: '',
       pass: ''
